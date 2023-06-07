b25123ad2c29c124bf87ee6d21227ab14b8feffa
Miguel Ron <xronsx@gmail.com>
2023-06-06 18:23:04 -0600

    Update config.h

--- keyboards/crkbd/keymaps/xronsx/config.h
+++ keyboards/crkbd/keymaps/xronsx/config.h
@@ -25,32 +25,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 
 //#define MASTER_LEFT
- #define MASTER_RIGHT
- //#define EE_HANDS
+#define MASTER_RIGHT
 
 // #define SSD1306OLED
-#undef USE_I2C
-#undef SSD1306OLED
+//#undef USE_I2C
+//#undef SSD1306OLED
 
-#define USE_SERIAL_PD2
+//#define USE_SERIAL_PD2
 
 // #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 150
-#define RETRO_TAPPING
-#define IGNORE_MOD_TAP_INTERRUPT
-
-#ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    #define RGBLED_NUM 27
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLIGHT_SLEEP
-    #define RGBLIGHT_SPLIT
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
-
+//#define TAPPING_TERM 150
+//#define RETRO_TAPPING
+//#define IGNORE_MOD_TAP_INTERRUPT
 
 #ifdef OLED_ENABLE
 #   define SPLIT_LAYER_STATE_ENABLE
@@ -64,53 +50,65 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef RGB_MATRIX_ENABLE
 #   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 // #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
-// #   define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
 #   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
 #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#   define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
-#   define RGB_MATRIX_HUE_STEP 8
-#   define RGB_MATRIX_SAT_STEP 8
-#   define RGB_MATRIX_VAL_STEP 8
-#   define RGB_MATRIX_SPD_STEP 10
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 60 
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8 
+#    define RGB_MATRIX_SPD_STEP 10
+/* Enable the animations you want/need.  You may need to enable only a small number of these because       *
+ * they take up a lot of space.  Enable and confirm that you can still successfully compile your firmware. */
+// RGB Matrix Animation modes. Explicitly enabled
+// For full list of effects, see:
+// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+//#    define ENABLE_RGB_MATRIX_ALPHAS_MODS
+//#    define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+//#    define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+//#    define ENABLE_RGB_MATRIX_BREATHING
+//#    define ENABLE_RGB_MATRIX_BAND_SAT
+//#    define ENABLE_RGB_MATRIX_BAND_VAL
+//#    define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+//#    define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+//#    define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+//#    define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+//#    define ENABLE_RGB_MATRIX_CYCLE_ALL
+//#    define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+//#    define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+//#    define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+//#    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+//#    define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+//#    define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+//#    define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+//#    define ENABLE_RGB_MATRIX_DUAL_BEACON
+//#    define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+//#    define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+//#    define ENABLE_RGB_MATRIX_RAINDROPS
+//#    define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+//#    define ENABLE_RGB_MATRIX_HUE_BREATHING
+//#    define ENABLE_RGB_MATRIX_HUE_PENDULUM
+//#    define ENABLE_RGB_MATRIX_HUE_WAVE
+//#    define ENABLE_RGB_MATRIX_PIXEL_RAIN
+//#    define ENABLE_RGB_MATRIX_PIXEL_FLOW
+//#    define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+// enabled only if RGB_MATRIX_FRAMEBUFFER_EFFECTS is defined
+//#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+//#    define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+// enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
+#    define ENABLE_RGB_MATRIX_SOLID_COLOR
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#    define ENABLE_RGB_MATRIX_SPLASH
+#    define ENABLE_RGB_MATRIX_MULTISPLASH
+#    define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#    define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
 
-/* Disable the animations you don't want/need.  You will need to disable a good number of these    *
- * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
-#   define ENABLE_RGB_MATRIX_ALPHAS_MODS
-#   define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-#    undef ENABLE_RGB_MATRIX_BREATHING
-#    undef ENABLE_RGB_MATRIX_BAND_SAT
-#    undef ENABLE_RGB_MATRIX_BAND_VAL
-#   define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-#    undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-#    undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-#    undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-#    undef ENABLE_RGB_MATRIX_CYCLE_ALL
-#    undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#    undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
-#    undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
-#    undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-#   define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-#    undef ENABLE_RGB_MATRIX_DUAL_BEACON
-#    undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
-#    undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL
-#    undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
-#    undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-#    undef ENABLE_RGB_MATRIX_RAINDROPS
-#    undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-#   define ENABLE_RGB_MATRIX_TYPING_HEATMAP
-#    undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#    undef ENABLE_RGB_MATRIX_SPLASH
-#    undef ENABLE_RGB_MATRIX_MULTISPLASH
-#    undef ENABLE_RGB_MATRIX_SOLID_SPLASH
-#    undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
 #endif
